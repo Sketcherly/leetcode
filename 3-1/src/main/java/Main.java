9d343b88fa8622045c7f2d2602783c8d0e65fd94
dongpo.li <dongpo.li@hotmail.com>
2025-04-17 18:53:26 +0800

    修改测试用例为例1

--- 3-1/src/main/java/Main.java
+++ 3-1/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String s = "au";
+        String s = "abcabcbb";
 
         int ans = new Solution().lengthOfLongestSubstring(s);
 
